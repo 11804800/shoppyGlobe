488febe2f880e957627fc3b5031a4da52746363c
Nikhil Pathak <nikhilpathak2602@gmail.com>
2024-10-06 15:24:11 +0530

    Not Found Page Component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,25 @@
-const NotFound=()=>{
-    return (
-        <div>Not Found Component</div>
-    )
+import { Link, useRouteError } from "react-router-dom";
+
+function NotFound() {
+
+  const err = useRouteError();
+
+  return (
+    <div className="error-container">
+      <div className="sub-container">
+        <h1  style={{ color: "rgb(1, 117, 172)" }}>
+          OPPS!
+        </h1>
+      </div>
+      <img src="/error.png" alt="error-image" className="error-img" />
+      <div className="sub-container">
+        <p>{err.data}</p>
+        <Link t="/" className="home-link">
+          HomePage
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default NotFound;
